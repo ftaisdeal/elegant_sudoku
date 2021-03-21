8779e5199600815c9ba63c3f6d63e1c01f70e44e
Firinn Taisdeal <firinn@taisdeal.com>
2021-03-21 18:26:41 -0400

    Minor edit to index.js

--- src/index.js
+++ src/index.js
@@ -136,7 +136,6 @@ class Game extends React.Component {
   }
 }
 
-
 // # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 ReactDOM.render(<Game />, document.getElementById("root"));
